a2203b648462b18fdc1c318102df5f554a638d7a
Ian Gonzalez <87671347+Ianrolando1253@users.noreply.github.com>
2023-08-07 19:30:10 -0400

    Creating function.js
    Solving basic Algebra with JavaScript and importing function.js into an HTML file. Genius!

---
+++ function.js
@@ -0,0 +1,13 @@
+
+function mathFunction() {
+    document.getElementById("demo").innerHTML = x + y; 
+}
+
+let x, y;
+
+x = 175;
+y = 55;
+
+mathFunction();
+
+// This right here is a basic math problem and solving it with JavaScript! //
